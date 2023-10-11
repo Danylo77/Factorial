7ded4a731ec986b8dca65519a926e4f8e85168a7
Danylo77 <dankoiniesta@gmail.com>
2023-10-11 16:51:35 +0300

    create app for main server

---
+++ MainServer/src/main/groovy/com/example/mainserver/MainServerApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.mainserver
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MainServerApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(MainServerApplication, args)
+    }
+
+}
